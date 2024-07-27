1eda8376e2414a646ce7116ae79d8bff5df012cd
Guilherme Nunes <guilherme.augd@gmail.com>
2024-07-26 21:56:22 -0300

    declare get number of lines function and set max line constant to three

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+MAX_LINES = 3
+
+
 def deposit():
     while True:
         amount = input("What would you like to deposit? $")
@@ -10,6 +13,9 @@ def deposit():
         else:
             print("Please enter a valid number.")
     return amount
+
+def get_number_of_lines():
+
 
 def main():
     balance = deposit()
