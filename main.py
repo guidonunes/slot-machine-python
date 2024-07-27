ffebb250f134dd93b7c2534a130509398e0156de
Guilherme Nunes <guilherme.augd@gmail.com>
2024-07-26 21:47:11 -0300

    implement deposit method

---
+++ main.py
@@ -0,0 +1,12 @@
+def deposit():
+    while True:
+        amount = input("What would you like to deposit? $")
+        if amount.isdigit():
+            amount = int(amount)
+            if amount > 0:
+                break
+            else:
+                print("Please enter a positive amount.")
+        else:
+            print("Please enter a valid number.")
+    return amount
