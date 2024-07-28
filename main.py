11dde42939a39a9050a6f37918704d76274a7119
Guilherme Nunes <guilherme.augd@gmail.com>
2024-07-28 12:01:17 -0300

    Generates a random spin for the slot machine

--- main.py
+++ main.py
@@ -11,8 +11,26 @@ symbol_count = {
     "A":2,
     "B":4,
     "C":6,
-    "D":8,
+    "D":8
 }
+
+# Generates a random spin for the slot machine
+def get_slot_machine_spin(rows, cols, symbols):
+    all_symbols =[]
+    for symbol, symbol_count in symbols.items():
+        for _ in range(symbol_count):
+            all_symbols.append(symbol)
+
+    columns = []
+    for _ in range(cols):
+        column = []
+        current_symbols = all_symbols[:]
+        for _ in range(rows):
+            value = random.choice(current_symbols)
+            current_symbols.remove(value)
+            column.append(value)
+
+        columns.append(column)
 
 # Handle's the user's deposit
 def deposit():
